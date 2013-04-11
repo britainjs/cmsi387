07b02eab5f126be0bdb262c379c971e0dfd1d8ed
Britainjs <bsouthwi@lion.lmu.edu>
2013-04-10 23:54:32 -0700

    Improved the error checking in the critical section.

--- homework/dining-philosophers/philo-cs.c
+++ homework/dining-philosophers/philo-cs.c
@@ -13,14 +13,15 @@ int getForks(int philosopher) {
     int leftFork = left(philosopher);
     int rightFork = right(philosopher);
     sem_wait(forks[leftFork]);
-    if(!forkStatus[leftFork]) {
+    if(forkStatus[leftFork] < 1) {
         printf("*** CRITICAL SECTION VIOLATION ***\n");
+        return -1;
     } else {
         forkStatus[leftFork] = 0;
     }
     printf("%s has fork %d\n", names[ philosopher ], forks[leftFork]);
     sem_wait(forks[rightFork]);
-    if(!forkStatus[rightFork]) {
+    if(forkStatus[rightFork] < 1) {
         printf("*** CRITICAL SECTION VIOLATION ***\n");
         return -1;
     } else {
@@ -33,10 +34,27 @@ int getForks(int philosopher) {
 int putForks(int i) {
     int leftFork = left(philosopher);
     int rightFork = right(philosopher);
+    
+    if(forkStatus[leftFork] > 0) {
+        printf("*** CRITICAL SECTION VIOLATION***\n");
+        return -1;
+    } else {
+        forkStatus[leftFork] = 1;
+    }
+    
     sem_post(forks[leftFork] );
     printf("%s has put down fork %d\n", names[philosopher], forks[leftFork]);
-    sem_post(forks[rightFork]);
+    
+    if(forkStatus[rightFork] > 0) {
+        printf("*** CRITICAL SECTION VIOLATION***\n");
+        return -1;
+    } else {
+        forkStatus[rightFork] = 1;
+    }
+    
+    sem_post(forks[rightFork]);    
     printf("%s has put down fork %d\n", names[philosopher], forks[rightFork]);
+    
     return 0;
 }
 
